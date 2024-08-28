6d1b986a8f7606a2b5aeec1dbcb6eb5838e9fa90
giovanna <torresssgio@gmail.com>
2024-08-28 17:51:36 -0300

    Separando chamada de api da main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 
-
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-function convertPokemonHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
     return ` <li class="pokemon">
                 <span class="number">#001</span>
                 <span class="name">${pokemon.name}</span>
@@ -20,19 +15,14 @@ function convertPokemonHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => 
+    pokeApi.getPokemons().then((pokemons) => 
         {
             for (let i = 0; i < pokemons.length; i++) {
                 const pokemon = pokemons[i];
-                pokemonList.innerHTML += convertPokemonHtml(pokemon)
+                pokemonList.innerHTML += convertPokemonToLi(pokemon)
             }
 
         })
-    .catch((error) => console.error(error))
 
 
 
